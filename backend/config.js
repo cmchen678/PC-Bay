a1ef289aa457280a20c64df482255495dd185c1b
cmchen678 <60148984+cmchen678@users.noreply.github.com>
2020-07-23 13:02:35 -0400

    moved env file

--- backend/config.js
+++ backend/config.js
@@ -2,6 +2,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+module.exports = {
+  mongoURI:
+    "mongodb+srv://Christian:belcross2yeah@cluster0.opd8z.mongodb.net/test?retryWrites=true&w=majority",
+};
+
 export default {
   MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost/pcbay",
   JWT_SECRET: process.env.JWT_SECRET || "somethingsecret",
