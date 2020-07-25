6d0cdf284a67640e46432dc289f56fb678c93cdc
cmchen678 <60148984+cmchen678@users.noreply.github.com>
2020-07-25 13:10:27 -0400

    added try catch to signin route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -25,20 +25,24 @@ router.put("/:id", isAuth, async (req, res) => {
 });
 
 router.post("/signin", async (req, res) => {
-  const signinUser = await User.findOne({
-    email: req.body.email,
-    password: req.body.password,
-  });
-  if (signinUser) {
-    res.send({
-      _id: signinUser.id,
-      name: signinUser.name,
-      email: signinUser.email,
-      isAdmin: signinUser.isAdmin,
-      token: getToken(signinUser),
+  try {
+    const signinUser = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
     });
-  } else {
-    res.status(401).send({ message: "Invalid Email or Password." });
+    if (signinUser) {
+      res.send({
+        _id: signinUser.id,
+        name: signinUser.name,
+        email: signinUser.email,
+        isAdmin: signinUser.isAdmin,
+        token: getToken(signinUser),
+      });
+    } else {
+      res.status(401).send({ message: "Invalid Email or Password." });
+    }
+  } catch (error) {
+    console.error(error);
   }
 });
 
